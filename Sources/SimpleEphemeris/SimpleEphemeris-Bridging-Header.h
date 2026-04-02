0962ff81e3c04d38373b81d315e4895ff7f2592b
Hugo Solis <hugo@solis.com>
2026-04-02 10:52:22 +0000

    Add more type definitions to bridging header

--- Sources/SimpleEphemeris/SimpleEphemeris-Bridging-Header.h
+++ Sources/SimpleEphemeris/SimpleEphemeris-Bridging-Header.h
@@ -9,6 +9,32 @@
 #define FALSE 0
 #endif
 
+#ifndef int32
+#define int32 int
+#endif
+
+#ifndef int64
+#define int64 long long
+#endif
+
+#ifndef uint32
+#define uint32 unsigned int
+#endif
+
+#ifndef uint64
+#define uint64 unsigned long long
+#endif
+
+#ifndef BYTE
+#define BYTE unsigned char
+#endif
+
+#ifndef DWORD
+#define DWORD unsigned int
+#endif
+
+#include <stdint.h>
+
 #import "sweph.h"
 #import "swephlib.h"
 #import "swedate.h"
